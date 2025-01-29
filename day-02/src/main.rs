574599d2e628ef4a11b4bf0c50453ba0fb69b10c
Simon Lomax <simx.99.99@gmail.com>
2025-01-29 11:28:20 +0000

    day-02 part 1 complete

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,3 +1,44 @@
+use utils::get_input;
+
 fn main() {
-    println!("Hello, world!");
+    let content = get_input();
+    let readings_count: i32 = get_readings(&content);
+    println!("Safe readings: {}", readings_count);
+}
+
+fn get_readings(content: &Vec<String>) -> i32 {
+
+    let mut readings_count: i32 = 0;
+    for line in content.iter() {
+        let levels: Vec<i32> = line.split(' ').map(|l| {
+            l.parse::<i32>().unwrap()
+        }).collect();
+        if check_safety(&levels) { readings_count += 1;}
+    }
+    readings_count
+}
+
+fn check_safety(levels: &Vec<i32>) -> bool {
+    let is_sorted = is_correctly_sorted(levels);
+    if !is_sorted { return false;}
+    levels.windows(2).all(|w| {
+        let diff = (w[0] - w[1]).abs();
+        diff > 0 && diff <= 3
+    })
+}
+
+fn is_correctly_sorted(levels: &Vec<i32>) -> bool {
+    let sorted_asc: Vec<i32> = {
+        let mut v = levels.clone();
+        v.sort();
+        v
+    };
+
+    // Descending order
+    let sorted_desc: Vec<i32> = {
+        let mut v = levels.clone();
+        v.sort_by(|a, b| b.cmp(a));
+        v
+    };
+    *levels == sorted_asc || *levels == sorted_desc
 }
