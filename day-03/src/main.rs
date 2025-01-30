c3a921a7e693dfe6db52bdce3ae561fb7c0e1f58
Simon Lomax <simx.99.99@gmail.com>
2025-01-30 14:29:14 +0000

    Day-03 part 1 complete

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,3 +1,27 @@
+use regex::Regex;
+use utils::get_input_as_string;
+
 fn main() {
-    println!("Hello, world!");
+    let content = get_input_as_string();
+    part_1(&content);
 }
+
+
+fn part_1(text: &str) {
+    let pattern = r"mul\((\d+,\d+)\)";
+
+    let re = Regex::new(pattern).unwrap();
+    let mut sum = 0;
+
+    for caps in re.captures_iter(text) {
+        if let Some(matched) = caps.get(1) { // Capture the "123,456" part
+            let parts: Vec<&str> = matched.as_str().split(',').collect(); // Split into ["123", "456"]
+
+            if let (Ok(first), Ok(second)) = (parts[0].parse::<i32>(), parts[1].parse::<i32>()) {
+                sum += first * second;
+            }
+        }
+    }
+    println!("Result = {}", sum);
+}
+
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -18,3 +18,13 @@ pub fn get_input() -> Vec<String> {
     });
     content.lines().map(|s| s.to_owned()).collect()
 }
+pub fn get_input_as_string() -> String {
+    let file_name = get_filename();
+    let content = fs::read_to_string(&file_name).unwrap_or_else(|_| {
+        panic!(
+            "Something went wrong reading the file: {}",
+            file_name.to_uppercase()
+        )
+    });
+    content
+}
